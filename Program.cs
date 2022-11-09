f76b2f2bd8d5e93581cf3832150435fd0403406a
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2022-11-09 09:25:58 -0300

    Organização por Pastas

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿namespace HUAWEI_2022___PROTOTYPE {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+    internal class Program{
+        static void Main(string[] args){
+            InitApp();
+        }
+        static void InitApp(){
+
         }
     }
 }
