20b439b29aa8551fe0ef0c340b9539d3114f0b49
Bárbara_Bueno <barbarabueno140104@gmail.com>
2022-11-16 09:33:30 -0300

    Running on port 8081

--- index.js
+++ index.js
@@ -3,15 +3,59 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.set("views", path.join(__dirname, "html"));
+app.set("views", path.join(__dirname, "Interfaces"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "html")));
+app.use(express.static(path.join(__dirname, "Interfaces")));
 
 
 // exibição da página inicial
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("areaCentral");
+});
+
+app.get("/areaCentral", function(req, res){
+    res.render("areaCentral");
+});
+
+app.get("/cadastroEmpresa", function(req, res){
+    res.render("cadastroEmpresa");
+});
+
+app.get("/cadastroEstudante", function(req, res){
+    res.render("cadastroEstudante");
+});
+
+app.get("/empresas", function(req, res){
+    res.render("empresas");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/perfil", function(req, res){
+    res.render("perfil");
+});
+
+app.get("/pesquisadores", function(req, res){
+    res.render("pesquisadores");
+});
+
+app.get("/processos_bu", function(req, res){
+    res.render("processos_bu");
+});
+
+app.get("/processos", function(req, res){
+    res.render("processos");
+});
+
+app.get("/processos2", function(req, res){
+    res.render("processos2");
+});
+
+app.get("/tipo_cad", function(req, res){
+    res.render("tipo_cad");
 });
 
 // conexão com banco de dados
